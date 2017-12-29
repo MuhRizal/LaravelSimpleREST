a1815eab3f14e8a8043861899cb361ccadd48e9d
Muh Rizal <info@muhrizal.com>
2017-12-29 14:29:41 +0700

    init commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+
+$router->get('/', function() use ($router) {
+    return "Lumen RESTful API By CoderExample (https://coderexample.com)";
+});
+ 
+$router->group(['prefix' => 'api/v1'], function($router)
+{
+    $router->get('book','BookController@index');
+  
+    $router->get('book/{id}','BookController@getbook');
+      
+    $router->post('book','BookController@createBook');
+      
+    $router->put('book/{id}','BookController@updateBook');
+      
+    $router->delete('book/{id}','BookController@deleteBook');
+});
